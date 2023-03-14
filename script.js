743f3365fe4caacda3514dfb1737f689ee123350
Mariana Lopes <mariana.siqueira2@fatec.sp.gov.br>
2023-03-13 22:24:30 -0300

    Ajustes

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if(hora < 6) {
+        img.src = ''
+    } else if (hora < 12) {
+        console.log('Bom dia!')
+    } else if (hora=12) {
+        console.log('Hora do almoço')
+    }else if (hora <= 18) {
+        console.log('Boa tarde!')
+    } else if (hora < 24) {
+        console.log('Boa noite!')
+    } else {
+        console.log('Por favor, insira uma hora válida.')
+    }
+}

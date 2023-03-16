6ccb577169992c454ade3a1fc1deb0b53cbd5f77
Mariana Lopes <mariana.siqueira2@fatec.sp.gov.br>
2023-03-16 19:13:46 -0300

    Background interativo

--- script.js
+++ script.js
@@ -2,18 +2,31 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
+    var hora = 21
+    var min = 10
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas`
+    var min = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora}:${min}.`
     if(hora < 6) {
-        img.src = ''
+        console.log('Boa madrugada...')
+        img.src = 'imagens/madrugada260.jpg'
+        document.body.style.background = '#283c43'
     } else if (hora < 12) {
         console.log('Bom dia!')
-    } else if (hora=12) {
+        img.src = 'imagens/manha260.jpg'
+        document.body.style.background = '#f2e5c0'
+    } else if (hora==12) {
         console.log('Hora do almoço')
+        img.src = 'imagens/almoço260.jpg'
+        document.body.style.background = '#b3c7a4'
     }else if (hora <= 18) {
         console.log('Boa tarde!')
+        img.src = 'imagens/pordosol260.jpg'
+        document.body.style.background = '#f7dc8c'
     } else if (hora < 24) {
         console.log('Boa noite!')
+        img.src = 'imagens/noite260.jpg'
+        document.body.style.background = '#2a1c68'
     } else {
         console.log('Por favor, insira uma hora válida.')
     }
